0e6a2c5b3d1d28d0a49d354e41ae3d64604e950a
Radhika-Gokhale <144609709+Radhika-Gokhale@users.noreply.github.com>
2024-06-21 11:03:42 +0530

    Update App.jsx

--- Employee_Frontend/src/App.jsx
+++ Employee_Frontend/src/App.jsx
@@ -1,44 +1,4 @@
 
-// import React from 'react';
-// import "./App.css"
-// // import Form from "./Form.jsx";
-// // import CommentsForm from "./CommentsForm.jsx";
-// // import TodoList from "./TodoList.jsx";
-// // import LudoBoard from "./LudoBoard.jsx";
-// // import LikeButton from "./LikeButton.jsx";
-// // import Counter from "./Counter.jsx";
-// // import EmployeeForm from "./EmployeeForm.js";
-// import EmployeeForm1212 from "./EmployeeForm";
-// function App() {
- 
-
-//   return (
-//     <>
-
-
-// <div className="App">
-//       <h1>Employee Form</h1>
-//       {/* <EmployeeForm /> */}
-//       <EmployeeForm1212 />
-//     </div>
-//     {/* <CommentsForm/> */}
-//     {/* <Form/> */}
-//     {/* <TodoList/> */}
-//     {/* <LudoBoard/> */}
-//     {/* <h3>States in React</h3>
-//     <Counter/> */}
-
-//     </>
-//   )
-// }
-
-// export default App
-
-
-
-
-
-// src/App.jsx
 
 import React from 'react';
 import "./App.css";
